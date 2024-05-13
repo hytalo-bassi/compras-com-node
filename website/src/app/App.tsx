e9902f1a4ed39c34db2eb1045a903402b52823c1
Hytalo Bassi <hytalo-bassi@protonmail.com>
2024-05-13 19:34:59 -0400

    feat: roteadores e Home

--- website/src/app/App.tsx
+++ website/src/app/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <nav>
+        <span>comno</span>
+        <ol>
+          <li>home</li>
+          <li>compras</li>
+          <li>painel</li>
+        </ol>
+      </nav>
+      <div className="App">
+        <Outlet />
+      </div>
+    </>
   );
 }
 
--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './app/home/Home';
+
+const router = createBrowserRouter(([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      }
+    ]
+  }
+]))
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
